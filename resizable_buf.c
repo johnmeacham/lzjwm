8457f949a30f51ae9ee9cad4e8ad9343378d2005
John <john@repetae.net>
2021-12-10 18:56:21 -0800

    update resizable_buf with newest version from https://github.com/johnmeacham/resizable_buf

--- resizable_buf.c
+++ resizable_buf.c
@@ -1,17 +1,30 @@
-#include <malloc.h>
 #include <assert.h>
-#include <string.h>
-#include <stdbool.h>
 #include <err.h>
+#include <malloc.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "resizable_buf.h"
 
+#ifndef NDEBUG
 #define NDEBUG
+#endif
+
+// don't make buffer smaller than this when freeing memory.
+#define MIN_SIZE 32
 
 extern inline void *rb_ptr(const rb_t *rb);
 extern inline void *rb_endptr(const rb_t *rb);
-extern inline unsigned rb_len(const rb_t *rb);
-extern inline void rb_clear(rb_t *rb);
+extern inline int rb_len(const rb_t *rb);
+extern inline int rb_red_zone(const rb_t *rb);
+
+//extern inline void rb_clear(rb_t *rb);
+
+extern inline int fifo_len(const fifo_t *fifo);
+extern inline void *fifo_head(const fifo_t *fifo);
+extern inline bool fifo_is_empty(const fifo_t *fifo);
+extern inline void fifo_discard(fifo_t *fifo);
 
 void rb_free(rb_t *rb)
 {
@@ -20,15 +33,18 @@ void rb_free(rb_t *rb)
 }
 
 /* ensure there are at least sz bytes past current length */
-static void
+void
 rb_grow(rb_t *rb, size_t sz)
 {
         assert(rb->len <= rb->size);
         assert(!rb->size || rb->buf);
-        while (rb->len + sz > rb->size)
-                rb->size = rb->size + (rb->size >> 1) + 8;
-        rb->buf = realloc(rb->buf, rb->size);
-        assert(rb->buf);
+        unsigned osz = rb->size;
+        while (rb->len + sz > osz)
+                osz = osz + (osz >> 1) + 8;
+        if (osz != rb->size) {
+                rb->size = osz;
+                rb->buf = realloc(rb->buf, rb->size);
+        }
         assert(rb->len <= rb->size);
         assert(!rb->size || rb->buf);
 }
@@ -69,6 +85,15 @@ rb_resize_fill(rb_t *rb, size_t len, char fillvalue)
         assert(rb->len <= rb->size);
 }
 
+void *
+rb_calloc(rb_t *rb, size_t len)
+{
+        return memset(rb_push(rb, len), 0, len);
+}
+
+/* parameters are reversed from usual to mimic c putc, this allows passing it to
+ * functions that expect a putc interface.
+ */
 int rb_putc(char ch, rb_t *rb)
 {
         rb_grow(rb, 1);
@@ -77,64 +102,140 @@ int rb_putc(char ch, rb_t *rb)
         return ch;
 }
 
-void
-rb_append(rb_t *rb, void *data, size_t len)
+/* append a unicode codepoint onto the buffer encoded in utf8. returns the
+ * number of bytes appended. */
+int rb_putwc(uint32_t wc, rb_t *rb)
 {
-        rb_grow(rb, len);
-        memcpy((char *)rb->buf + rb->len, data, len);
-        rb->len += len;
-        assert(rb->len <= rb->size);
+        rb_grow(rb, 4);
+        uint8_t *s = rb_endptr(rb);
+        int i = 0;
+        if (wc <= 0x7f) {
+                s[i++] = (uint8_t)wc;
+        } else {
+                if (wc <= 0x7ff) {
+                        s[i++] = (uint8_t)((wc >> 6) | 0xc0);
+                } else {
+                        if (wc <= 0xffff) {
+                                s[i++] = (uint8_t)((wc >> 12) | 0xe0);
+                        } else {
+                                s[i++] = (uint8_t)((wc >> 18) | 0xf0);
+                                s[i++] = (uint8_t)(((wc >> 12) & 0x3f) | 0x80);
+                        }
+                        s[i++] = (uint8_t)(((wc >> 6) & 0x3f) | 0x80);
+                }
+                s[i++] = (uint8_t)((wc & 0x3f) | 0x80);
+        }
+        rb->len += i;
+        return i;
 }
 
-void
+
+void *
+rb_append(rb_t *rb, void *data, size_t len)
+{
+        return memcpy(rb_push(rb, len), data, len);
+}
+
+void *
 rb_set(rb_t *rb, void *data, size_t len)
 {
         rb_makesize(rb, len, false);
-        memcpy(rb->buf, data, len);
         rb->len = len;
         assert(rb->len <= rb->size);
+        return memcpy(rb->buf, data, len);
 }
 
-void
+char *
 rb_strcpy(rb_t *rb, char *str)
 {
         rb_set(rb, str, strlen(str));
-        rb_stringize(rb);
+        return rb_stringize(rb);
 }
 
-void
+char *
 rb_strcat(rb_t *rb, char *str)
 {
-        rb_append(rb, str, strlen(str));
+        char *res = rb_append(rb, str, strlen(str));
         rb_stringize(rb);
+        return res;
 }
 
-void
+/* similar to rb_strcat but returns number of bytes copied rather than pointer
+ * to destination, follows stdio argument convention */
+int
+rb_puts(char *str, rb_t *rb)
+{
+        int len = strlen(str);
+        rb_append(rb, str, len);
+        rb_stringize(rb);
+        return len;
+}
+
+/* Copy part of rb2 and append it to rb, returns a pointer into rb's buffer where the
+ * extracted data lives. */
+void *
 rb_extract(rb_t *rb, const rb_t *rb2, unsigned loc, size_t len)
 {
-        rb_set(rb, (char *)rb_ptr(rb2) + loc, len);
+        ssize_t nlen = len;
+        if (loc + len > rb->len)
+                nlen -= loc + len - rb->len;
+        if (nlen <  0)
+                return rb_endptr(rb);
+        return rb_append(rb, (char *)rb_ptr(rb2) + loc, len);
 }
 
-void
+/* insert space into a resizable buf moving existing data or extending the size
+ * of the buf if you are inserting space past the end. new space will be
+ * uninitialized.
+ * follow with rb_memset to initialize the space to a value if you require it.
+ *
+ * one past the end of the buffer is moved as well to preserve stringification.
+ */
+void *
 rb_insert_space(rb_t *rb, unsigned loc, size_t len)
 {
         assert(rb->len <= rb->size);
         assert((rb->buf && rb->size) || (!rb->buf && !rb->size));
-        assert(loc <= rb->len);
-        rb_grow(rb, len);
-        memmove((char *)rb->buf + loc + len, (char *)rb->buf + loc,
-                rb->len - loc);
-        rb->len += len;
+        if (loc + len > rb->len) {
+                /* if we are inserting past the end, no need to memmove any data
+                 * around */
+                rb_resize(rb, loc + len, true);
+        } else {
+                rb_grow(rb, len + 1);
+                memmove((char *)rb->buf + loc + len, (char *)rb->buf + loc,
+                        rb->len - loc + 1);
+                rb->len += len;
+        }
         assert(rb->len <= rb->size);
         assert((rb->buf && rb->size) || (!rb->buf && !rb->size));
+        return rb_ptr(rb) + loc;
 }
 
-void
+/* set memory to the specific value, will grow buffer as needed to contain the
+ * range specified. loc may be beyond the end of the buffer.
+ * returns pointer to the beginning of the changed memory. */
+void *
+rb_memset(rb_t *rb, char what, unsigned loc, size_t len)
+{
+        if (loc + len > rb->len)
+                rb_resize(rb, loc + len, true);
+        return memset(rb_ptr(rb) + loc, what, len);
+}
+
+
+/* insert some data at the given location moving data as needed to make space.
+ * if loc is beyond the end of the buffer, uninitialized space will be added
+ * to the hole. one past the end of the buffer is also moved to preserve
+ * terminators. */
+void *
 rb_insert(rb_t *rb, unsigned loc, char *data, size_t len)
 {
-        rb_insert_space(rb, loc, len);
-        memcpy((char *)rb->buf + loc, data, len);
+        return memcpy(rb_insert_space(rb, loc, len), data, len);
 }
+
+/* delete some data from inside a buffer, moving any trailing data as needed. if
+ * loc is greater than the size of the array then nothing happens.
+ * when moving data one char past the end is moved to preserve null terminators. */
 
 void
 rb_delete(rb_t *rb, unsigned loc, size_t len)
@@ -142,18 +243,29 @@ rb_delete(rb_t *rb, unsigned loc, size_t len)
         assert(loc <= rb->len);
         assert(rb->len <= rb->size);
         assert((rb->buf && rb->size) || (!rb->buf && !rb->size));
-        memmove((char *)rb->buf + loc, (char *)rb->buf + loc + len,
-                rb->len - (loc + len));
-        rb->len -= len;
+        if (loc + len > rb->len) {
+                if (loc < rb->len)
+                        rb->len = loc;
+        } else {
+                rb_grow(rb, 1);
+                memmove((char *)rb->buf + loc, (char *)rb->buf + loc + len,
+                        rb->len - (loc + len) + 1);
+                rb->len -= len;
+        }
         assert(rb->len <= rb->size);
         assert((rb->buf && rb->size) || (!rb->buf && !rb->size));
 }
 
-void
+/* make sure data is null terminated without actually increasing its length. so
+ * you can pass it to routines that expect null terminated data but the length
+ * still reflects the length of the data. This is idempotent and will not grow
+ * the buffer if it isn't needed. */
+char *
 rb_stringize(rb_t *rb)
 {
         rb_grow(rb, 1);
         ((char *)rb->buf)[rb->len] = '\0';
+        return rb_ptr(rb);
 }
 
 void *rb_peek(rb_t *rb, int n)
@@ -162,6 +274,29 @@ void *rb_peek(rb_t *rb, int n)
                 return rb_endptr(rb) - n;
         } else
                 return NULL;
+}
+
+void *rb_assign(rb_t *dst, rb_t *src)
+{
+        rb_free(dst);
+        *dst = *src;
+        *src = (rb_t)RB_BLANK;
+        return rb_ptr(dst);
+}
+
+void rb_swap(rb_t *x, rb_t *y)
+{
+        rb_t t = *x;
+        *x = *y;
+        *y = t;
+}
+
+/* take over ownership of the malloc'ed buffer */
+void *rb_take(rb_t *rb)
+{
+        void *r = rb_ptr(rb);
+        *rb = (rb_t)RB_BLANK;
+        return r;
 }
 
 void *rb_pop(rb_t *rb, int n)
@@ -180,18 +315,30 @@ void *rb_push(rb_t *rb, int n)
         return ret;
 }
 
-void
+/* the pointer returned becomes invalid after any fifo mutation */
+void *
 fifo_append(fifo_t *fifo, void *data, size_t len)
 {
-        if (!fifo->offset || len < fifo->rb.size - fifo->rb.len) {
-                rb_append(&fifo->rb, data, len);
-        } else {
-                memmove(fifo->rb.buf, fifo->rb.buf + fifo->offset,
-                        FIFO_LEN(*fifo));
+        if (!(!fifo->offset || len < fifo->rb.size - fifo->rb.len)) {
+                // TODO we should grow rather than move up to a certain point.
+                memmove(fifo->rb.buf, fifo->rb.buf + fifo->offset, fifo_len(fifo));
                 fifo->rb.len -= fifo->offset;
                 fifo->offset = 0;
-                rb_append(&fifo->rb, data, len);
         }
+        return rb_append(&fifo->rb, data, len);
+}
+
+/* the pointer returned becomes invalid after any fifo mutation */
+void *fifo_dequeue(fifo_t *fifo, size_t len)
+{
+        void *res = fifo_head(fifo);
+        if (len > fifo_len(fifo))
+                return NULL;
+        if (len == fifo_len(fifo))
+                fifo_discard(fifo);
+        else
+                fifo->offset += len;
+        return res;
 }
 
 
@@ -201,16 +348,16 @@ fifo_append(fifo_t *fifo, void *data, size_t len)
 ssize_t
 rb_fread(rb_t *rb, FILE *fh, size_t n)
 {
-        size_t tr = 0;
-        while (!feof(fh)) {
+        ssize_t tr = 0;
+        while (!feof(fh) && tr <= n) {
                 char buf[4096];
-                size_t res = fread(buf, 1, __MIN(n, sizeof(buf)), fh);
-                if (!res)
-                        return -(tr + 1);
+                size_t res = fread(buf, 1, __MIN(n - tr, sizeof(buf)), fh);
                 rb_append(rb, buf, res);
                 tr += res;
-                if (tr >= n)
+                if (ferror(fh)) {
+                        tr =  - (tr + 1);
                         break;
+                }
         }
         rb_stringize(rb);
         return tr;
@@ -226,10 +373,10 @@ rb_fwrite(rb_t *rb, FILE *fh, size_t n)
         size_t tw = 0;
         while (n > 0) {
                 size_t res = fwrite(rb_ptr(rb) + tw, 1, n, fh);
-                if (!res && ferror(fh))
-                        return -(tw + 1);
                 tw += res;
                 n -= res;
+                if (ferror(fh))
+                        return -(tw + 1);
         }
         return tw;
 }
@@ -248,6 +395,7 @@ rb_read_file(rb_t *rb, char *fname)
         return res;
 }
 
+
 ssize_t
 rb_write_file(rb_t *rb, char *fname, bool append)
 {
@@ -258,3 +406,53 @@ rb_write_file(rb_t *rb, char *fname, bool append)
         }
         return rb_fwrite(rb, fh, -1);
 }
+
+int
+rb_printf(rb_t *rb, char *fmt, ...)
+{
+        va_list ap;
+        va_start(ap, fmt);
+        int size = vsnprintf(NULL, 0, fmt, ap);
+        va_end(ap);
+        if (size < 0)
+                return size;
+        rb_grow(rb, size + 1);
+        va_start(ap, fmt);
+        size = vsnprintf(rb->buf + rb->len, size + 1, fmt, ap);
+        va_end(ap);
+        if (size < 0)
+                return size;
+        rb->len += size;
+        return size;
+}
+
+// unit tests
+#if 0
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+
+int main(int argc, char *argv[])
+{
+        rb_t rb = RB_BLANK;
+        assert(!rb_ptr(&rb));
+        assert(!rb_endptr(&rb));
+        assert(!rb_len(&rb));
+        rb_printf(&rb, "hello %i %sX", 43, "test");
+        assert(*RBP(char, &rb) == 'h');
+        assert(*RBPE(char, &rb) == 0);
+        assert(rb_len(&rb) == 14);
+        /* lets pad out with non nulls */
+        rb_grow(&rb, 15);
+        memset(RBPE(char, &rb) + 1, 'x', 14);
+        rb_insert(&rb, 5, ", world", 7);
+        puts(rb_ptr(&rb));
+        assert(!strcmp(rb_ptr(&rb), "hello, world 43 testX"));
+        rb_delete(&rb, 2, 7);
+        puts(rb_ptr(&rb));
+        rb_delete(&rb, 10, 2);
+        puts(rb_ptr(&rb));
+        assert(!strcmp(rb_ptr(&rb), "herld 43 ttX"));
+        return 0;
+}
+#endif
--- resizable_buf.h
+++ resizable_buf.h
@@ -5,22 +5,23 @@
  * these grow as needed with sublinear cost with minimal overhead. */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <inttypes.h>
 
 typedef struct rb rb_t;
-typedef struct fifo fifo_t;
 
 struct rb {
         void *buf;
-        unsigned len;
-        unsigned size;
+        int len;
+        int size;
 };
 
 /* This should be used to initialize new buffers.
  * rb_t rb = RB_BLANK;
+ * zero initialized memory also works.
  */
 #define RB_BLANK        {NULL, 0, 0}
-
 
 /*
  * The macro versions accept a type as an argument and cast results
@@ -30,7 +31,7 @@ struct rb {
  * The macro versions can also be used as lvalues for setting entries.
  *
  * example:
- * int *ints = RBP(int, &rb)
+ * int *iptr = RBP(int, &rb)
  *
  * */
 
@@ -46,10 +47,34 @@ struct rb {
  * RB_PUSH(long long, &rb) = 389;
  *
  */
-#define RB_PEEK(t,rb)      *(t *)(rb_peek(rb, sizeof(t)))
-#define RB_POP(t,rb)       *(t *)(rb_pop(rb, sizeof(t)))
-#define RB_PUSH(t,rb)      *(t *)(rb_push(rb, sizeof(t)))
 #define RB_PUSHN(t,rb,n)   (t *)(rb_push(rb, (n)*sizeof(t)))
+#define RB_POPN(t,rb,n)    (t *)(rb_pop(rb, (n)*sizeof(t)))
+#define RB_PEEKN(t,rb,n)   (t *)(rb_pop(rb, (n)*sizeof(t)))
+
+#define RB_PEEK(t,rb)      *RB_PEEKN(t,rb,1)
+#define RB_POP(t,rb)       *RB_POPN(t,rb,1)
+#define RB_PUSH(t,rb)      *RB_PUSHN(t,rb,1)
+
+
+// these require the argument be addressable but don't need the type explicitly
+// passed.
+#define RB_LPUSH(rb, x)      rb_append(rb, &(x), sizeof(x))
+#define RB_LPUSHN(rb, x, n)  rb_append(rb, x, sizeof(*(x))*(n))
+
+#define RB_LPOP(v, rb)       memcpy(&(v), rb_pop(rb, sizeof(v)))
+#define RB_LPOPN(v, rb, n)   memcpy(v, rb_pop(rb, (n)*sizeof(*(v))))
+#define RB_LPEEK(v, rb)      memcpy(&(v), rb_peek(rb, sizeof(v)))
+#define RB_LPEEKN(v, rb, n)  memcpy(v, rb_peek(rb, (n)*sizeof(*(v))))
+
+// these assume v is a ptr and assign it to directly point at the value in the
+// rb
+#define RB_APOPN(v, rb, n)   do { v = rb_pop(rb, (n)*sizeof(*(v))); } while (0)
+#define RB_APEEKN(v, rb, n)  do { v = rb_peek(rb, (n)*sizeof(*(v))); } while (0)
+
+/* sort and search */
+typedef int (*rb_cmpfunc)(const void *, const void *);
+#define RB_QSORT(t,rb,cfunc) qsort(rb_ptr(rb), RB_NITEMS(rb), (rb_cmpfunc)(cfunc))
+#define RB_BSEARCH(t,k,rb,cfunc) bsearch(k, rb_ptr(rb), RB_NITEMS(rb), (rb_cmpfunc)(cfunc))
 
 /*
  * for loop over rb values.
@@ -59,7 +84,11 @@ struct rb {
  *     sum += *ptr;
  * }
  */
-#define RB_FOR(t,v,rb)     for (t *v = rb_ptr(rb); (void *)v < rb_ptr(rb) + rb_len(rb); v++)
+#define RB_FOR(t,v,rb)  for (t *v = rb_ptr(rb); (void *)v < rb_endptr(rb); v++)
+
+/* this iterates over pairs of index,value so you can get at the index too. This
+ * also allows for the underlying buffer to be mutated. */
+#define RB_FOR_ENUM(t,var,rb)  for (struct { int k; t *v; } var = { .k = 0, .v = RBP(t,rb) }; var.k < (int)RB_NITEMS(t, rb); var.k++, var.v = RBP(t,rb) + var.k)
 
 inline void *rb_ptr(const rb_t *rb)
 {
@@ -69,28 +98,65 @@ inline void *rb_endptr(const rb_t *rb)
 {
         return rb->buf + rb->len;
 }
-inline unsigned rb_len(const rb_t *rb)
+inline int rb_len(const rb_t *rb)
 {
         return rb->len;
 }
-inline void rb_clear(rb_t *rb)
+/* how much space we can safely use past the end of the buffer */
+inline int rb_red_zone(const rb_t *rb)
 {
-        rb->len = 0;
+        return rb->size - rb->len;
 }
 
-void *rb_pop(rb_t *rb, int n);
-void *rb_peek(rb_t *rb, int n);
-void *rb_push(rb_t *rb, int n);
+/* clears rb to the empty buffer and frees all memory associated with it. */
 void rb_free(rb_t *rb);
-void rb_append(rb_t *rb, void *data, size_t len);
-void rb_set(rb_t *rb, void *data, size_t len);
-void rb_extract(rb_t *rb, const rb_t *rb2, unsigned loc, size_t len);
-void rb_insert(rb_t *rb, unsigned loc, char *data, size_t len);
-void rb_insert_space(rb_t *rb, unsigned loc, size_t len);
+
+/* ensure there is a nul padding byte after the end of the data without
+ * increasing its length. useful for passing data to routines that expect nul
+ * terminated strings. */
+char *rb_stringize(rb_t *rb);
+
+/* append data to a buffer, return the location of the appended data */
+void *rb_append(rb_t *rb, void *data, size_t len);
+
+/* extract data from one buffer and apend it to another, returns the address of
+ * the newly extracted data. will copy what it can if an invalid range is
+ * selected. If you need to know how many bytes were copied, check rb_len on the
+ * target buffer. */
+void *rb_extract(rb_t *rb, const rb_t *rb2, unsigned loc, size_t len);
+void *rb_insert(rb_t *rb, unsigned loc, char *data, size_t len);
+void *rb_insert_space(rb_t *rb, unsigned loc, size_t len);
+void *rb_memset(rb_t *rb, char what, unsigned loc, size_t len);
+void *rb_peek(rb_t *rb, int n);
+void *rb_pop(rb_t *rb, int n);
+void *rb_push(rb_t *rb, int n);
+void *rb_set(rb_t *rb, void *data, size_t len);
 void rb_delete(rb_t *rb, unsigned loc, size_t len);
-void rb_stringize(rb_t *rb);
 void rb_resize(rb_t *rb, size_t len, bool preserve);
 void rb_resize_fill(rb_t *rb, size_t len, char fillvalue);
+void *rb_calloc(rb_t *rb, size_t len);
+
+/* do not change length but make sure there are at least sz bytes in the redzone
+ * */
+void rb_grow(rb_t *rb, size_t sz);
+
+/* Do a zero copy transfer of data from one rb to another handling freeing
+ * memory properly.
+ *
+ * after this operation src will be a freed empty buffer, anything previously in
+ * dst will have been freed and dst wil contain what used to be in src.
+ *
+ * returns a pointer to the assigned data.
+ * */
+void *rb_assign(rb_t *dst, rb_t *src);
+
+/* swap the data between two buffers in place. */
+void rb_swap(rb_t *x, rb_t *y);
+
+/* take ownership of the rb buffer, this will return the malloc allocated buffer
+ * of rb and clear rb. It is the users responsibilty to free() the buffer when
+ * done. */
+void *rb_take(rb_t *rb);
 
 /* file operations.
  * These return the number of bytes copied or -(bytes copied + 1) on error. */
@@ -104,31 +170,68 @@ ssize_t rb_fread(rb_t *rb, FILE *fh, size_t n);
 
 /* arguments reversed to match putc */
 int rb_putc(char ch, rb_t *rb);
+int rb_puts(char *str, rb_t *rb);
+/* assumes unicode codepoints and utf8 encoding. */
+int rb_putwc(uint32_t, rb_t *rb);
 
 /* string operations, these operate with null terminated strings rather than
  * length delimited data. they always leve the data in the buffer null
  * terminated and return the number of characters operated on. the null
  * terminator is not counted as part of the length of the buffer just
  * like stringize */
-void rb_strcpy(rb_t *rb, char *str);
-void rb_strcat(rb_t *rb, char *str);
+char *rb_strcpy(rb_t *rb, char *str);
+char *rb_strcat(rb_t *rb, char *str);
+int rb_printf(rb_t *rb, char *fmt, ...);
+
+/* specialty routines */
+
+/* extended routines, these have a callback that will be called if the
+ * underlying memory is reallocated to a new region */
+void *rb_appendx(rb_t *rb, void *data, size_t len, void (*)(void *, void *));
+void *rb_callocx(rb_t *rb, size_t len, void (*)(void *, void *));
+
 
 /* fifo are a first in first out buffer. these reset storage when the fifo is
  * empty, so only use when the fifo is periodically emptied.
  * they are just a resizable buffer with an offset into it. */
+
+typedef struct fifo fifo_t;
 
 struct fifo {
         struct rb rb;
         unsigned offset;
 };
 
-#define FIFO_PTR(f) (((f).rb).len + (f).offset)
-#define FIFO_LEN(f) (((f).rb).len - (f).offset)
-#define FIFO_EMPTY(f) (!FIFO_LEN(f))
 #define FIFO_BLANK  {RB_BLANK, 0}
-#define FIFO_POP(f, l) do { if (l == FIFO_LEN(f)) {(f).offset = 0; RB_CLEAR((f).rb); } else (f).offset += l;  } while (0)
-#define FIFO_CLEAR(f) do { (f).offset = 0; RB_CLEAR((f).rb);   } while (0)
 
-void fifo_append(fifo_t *fifo, void *data, size_t len);
+inline int fifo_len(const fifo_t *fifo)
+{
+        return fifo->rb.len - fifo->offset;
+}
+inline void *fifo_head(const fifo_t *fifo)
+{
+        return rb_ptr(&fifo->rb) + fifo->offset;
+}
+inline bool fifo_is_empty(const fifo_t *fifo)
+{
+        return !fifo_len(fifo);
+}
+
+/* discard any pending data. this does not free memory. */
+inline void fifo_discard(fifo_t *fifo)
+{
+        fifo->rb.len = 0;
+        fifo->offset = 0;
+}
+inline void fifo_free(fifo_t *fifo)
+{
+        rb_free(&fifo->rb);
+        fifo->offset = 0;
+}
+
+// #define FIFO_POP(f, l) do { if (l == FIFO_LEN(f)) {(f).offset = 0; RB_CLEAR((f).rb); } else (f).offset += l;  } while (0)
+
+void *fifo_append(fifo_t *fifo, void *data, size_t len);
+void *fifo_dequeue(fifo_t *fifo, size_t len);
 
 #endif
